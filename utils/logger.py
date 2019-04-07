fdc24c323394aea2c1ae308001482c3d323e3b0b
snowp <snowpff14@gmail.com>
2019-04-07 19:02:50 +0900

    おかしい状態なので修正

--- utils/logger.py
+++ utils/logger.py
@@ -45,28 +45,6 @@ class LoggerObj:
         self.loggers[name]=self.logger
         return self.logger
 
-    def __init__(self, name=__name__,logFilename='log_'):
-        self.logger = getLogger(name)
-        self.logger.setLevel(DEBUG)
-        formatter = Formatter("[%(levelname)s]:[%(asctime)s]: %(message)s")
-        needoutput=True
-        # stdout
-        handler = StreamHandler()
-        handler.setLevel(DEBUG)
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
-        logdirectory='logs'
-        os.makedirs(logdirectory,exist_ok=True)
-        fileNamePath=logdirectory+'/'+logFilename
-        if needoutput:
-            logFileName=fileNamePath+datetime.now().strftime("%Y%m%d%H%M%S")+'.log'
-            # file
-            handler = handlers.RotatingFileHandler(filename = logFileName,
-                                                maxBytes = 1048576,
-                                                backupCount = 3)
-            handler.setLevel(DEBUG)
-            handler.setFormatter(formatter)
-            self.logger.addHandler(handler)
 
     def debug(self, msg):
         self.logger.debug(msg)
